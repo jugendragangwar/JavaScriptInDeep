73bca54145e2b90825b607524919aee22ac307dd
jugendragangwar <jugendragangwar8791@gmail.com>
2025-07-22 19:18:46 +0530

    Solve a aaray question(swap first half of Array with its second half)

--- 06_Array/Solution5.js
+++ 06_Array/Solution5.js
@@ -1,8 +1,19 @@
 // WAP to accept 12 numbers in an array and swap first half of Array with its second half.
 
 function swapArray(array) {
-  let lenght = array.length / 2;
-  console.log(lenght);
+  let firstHalf = [];
+  let secondHalf = [];
+
+  let mid = array.length / 2;
+
+  for (let i = 0; i < mid; i++) {
+    firstHalf.push(array[i]);
+  }
+  for (let i = mid; i < array.length; i++) {
+    secondHalf.push(array[i]);
+  }
+  let swappedArray = secondHalf.concat(firstHalf);
+  console.log(swappedArray, "swappedArray");
 }
 
 const arrayNum = [12, 33, 65, 65, 87, 12, 76, 44, 77, 86, 34, 65];
