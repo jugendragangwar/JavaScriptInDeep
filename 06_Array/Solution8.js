94826caf3a878e8713d34a317fd76266feb9e3d3
jugendragangwar <jugendragangwar8791@gmail.com>
2025-08-26 21:17:41 +0530

    Solve Array question

---
+++ 06_Array/Solution8.js
@@ -0,0 +1,23 @@
+// WAP to accept 10 numbers in an array. Accept a position from user and delete the element at that position from Array.
+
+function deleteItemInArray(userArray, position) {
+  let result = [];
+  for (let i = 0; i < userArray.length; i++) {
+    if (i !== position - 1) {
+      result.push(userArray[i]);
+    }
+  }
+  return result;
+}
+
+let arr = [12, 33, 54, 67, 87, 9, 32, 43, 45, 32];
+let userPosition = 5;
+
+let updatedArray = deleteItemInArray(arr, userPosition);
+console.log(updatedArray);
+
+// By splice method
+let array = [12, 33, 54, 67, 87, 9, 32, 43, 45, 32];
+let Position = 5;
+array.splice(userPosition-1, 1);
+console.log(array);
